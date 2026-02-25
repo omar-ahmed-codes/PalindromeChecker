33f706a459b6de358c56047fa90f8e3b8e17b6ed
omar-ahmed-codes <omarsayeed121@gmail.com>
2026-02-25 10:07:39 +0530

    UC3: Hardcoded and reversed palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,22 +1,11 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-
-        // Hardcoded string
         String input = "madam";
-
-        // Assume it is a palindrome initially
-        boolean isPalindrome = true;
-
-        // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-        // Print result
-        if (isPalindrome) {
+        if (input.equals(reversed)) {
             System.out.println("The string \"" + input + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
