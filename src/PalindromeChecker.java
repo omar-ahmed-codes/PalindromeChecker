2cc093b824f598af65b82d31aef340221453355e
omar-ahmed-codes <omarsayeed121@gmail.com>
2026-02-25 10:51:16 +0530

    UC13: Performance Comparison

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,29 +1,23 @@
 import java.util.Stack;
-import java.util.Deque;
-import java.util.ArrayDeque;
 
-public class PalindromeChecker {
+public class PalindromeChecker{
 
     public static void main(String[] args) {
 
         String input = "level";
 
-        PalindromeStrategy strategy = new StackStrategy();
+        long startTime = System.nanoTime();
+        boolean result = checkUsingStack(input);
+        long endTime = System.nanoTime();
 
-        boolean result = strategy.check(input);
+        long executionTime = endTime - startTime;
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
-}
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
+    public static boolean checkUsingStack(String input) {
 
         Stack<Character> stack = new Stack<>();
 
@@ -33,26 +27,6 @@ class StackStrategy implements PalindromeStrategy {
 
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.add(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
                 return false;
             }
         }
