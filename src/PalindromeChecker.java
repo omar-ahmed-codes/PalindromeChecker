01273ff9ee6b4ac864d299966d2efd231540d4ee
Omar Ahmed <omarsayeed121@gmail.com>
2026-02-25 10:02:47 +0530

    Refactor PalindromeChecker for clarity

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,13 +1,13 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
 
-        // Hardcoded string
+        
         String input = "madam";
 
-        // Assume it is a palindrome initially
+       
         boolean isPalindrome = true;
 
-        // Loop only till half of the string length
+        
         for (int i = 0; i < input.length() / 2; i++) {
 
             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
@@ -15,7 +15,7 @@ public class PalindromeChecker {
                 break;
             }
         }
-        // Print result
+       
         if (isPalindrome) {
             System.out.println("The string \"" + input + "\" is a Palindrome.");
         } else {
