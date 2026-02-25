24a7c612443f753e4b8c01c1dbf320a3a32ab09f
omar-ahmed-codes <omarsayeed121@gmail.com>
2026-02-25 09:35:27 +0530

    UC1: Add welcome message module and version

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,6 @@
+public class PalindromeChecker {
+    public static void main(String [] args){
+        System.out.println("Welcome to Palindrome Checker!");
+        System.out.println("Version~1.0");
+    }
+}
